d936ffa8c00fa7bd8eeb1392da0bef1a4c9c9d69
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2025-01-16 14:43:41 +0330

    Initial commit

---
+++ src/main/java/com/example/webphase3/WebPhase3Application.java
@@ -0,0 +1,13 @@
+package com.example.webphase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPhase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPhase3Application.class, args);
+	}
+
+}
